06d88336cda8f467a667834f273ffee53adf5d1a
Koketso-dax <lanchaster.k@gmail.com>
2024-02-29 11:39:48 +0200

    Fixed 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,8 +14,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 		return (0);
 	}
 
-	left_h = binary_tree_height(tree->left);
-	right_h = binary_tree_height(tree->right);
+	left_h = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	right_h = tree-> right ? 1 + binary_tree_height(tree->right) : 0;
 
-	return ((left_h > right_h) ? (left_h + 1) : (right_h + 1));
+	return (left_h > right_h ? left_h : right_h);
 }

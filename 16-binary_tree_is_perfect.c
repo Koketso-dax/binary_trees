0c4978db931c5ed68f658692902189bd0629d96a
Koketso-dax <lanchaster.k@gmail.com>
2024-02-29 12:47:59 +0200

    Fixed 16

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -65,3 +65,23 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	}
 	return (0);
 }
+/**
+ * binary_tree_size - determines size of binary tree.
+ * @tree: input tree.
+ * Return: Tree size or 0 on NULL.
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t left_size = 0;
+	size_t right_size = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	left_size = binary_tree_size(tree->left);
+	right_size = binary_tree_size(tree->right);
+
+	return (left_size + right_size + 1);
+}

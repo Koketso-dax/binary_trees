a1f4a8eb687f1406d819a8e8a476b4952bf313ee
Koketso-dax <lanchaster.k@gmail.com>
2024-02-29 12:35:45 +0200

    I forgot what I fixed

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,5 +1,6 @@
 #include "binary_trees.h"
-
+size_t binary_tree_height(const binary_tree_t *tree);
+int binary_tree_is_full(const binary_tree_t *tree);
 /**
  * binary_tree_is_perfect - will check if binary tree is perfect.
  * @tree: input tree address.
@@ -8,12 +9,59 @@
 
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	size_t height = 0;
+
 	if (tree == NULL)
 	{
 		return (0);
 	}
-	size_t height = binary_tree_height(tree);
+	height = binary_tree_height(tree);
 
 	return (binary_tree_is_full(tree) &&
 			((size_t)1 << height) - 1 == binary_tree_size(tree));
+}
+/**
+ * binary_tree_height - height helper func
+ * @tree: input tree.
+ * Return: Binary tree height. 0 on Null.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height = 0;
+	size_t right_height = 0;
+
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
+
+	return ((left_height > right_height) ?
+			(left_height + 1) : (right_height + 1));
+}
+/**
+ * binary_tree_is_full - recursively determines if tree is full.
+ * @tree: input tree.
+ * Return: 0 or 1.
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+	{
+		return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+		return (1);
+	}
+
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		return (binary_tree_is_full(tree->left)
+				&& binary_tree_is_full(tree->right));
+	}
+	return (0);
 }

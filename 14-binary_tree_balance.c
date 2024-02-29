44426f6c27fd99b157a5edaa03da2565bd5963a6
Koketso-dax <lanchaster.k@gmail.com>
2024-02-29 10:38:31 +0200

    fixed 1-left

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -14,7 +14,8 @@ size_t binary_tree_heights(const binary_tree_t *tree)
 	size_t left_height = binary_tree_heights(tree->left);
 	size_t right_height = binary_tree_heights(tree->right);
 
-	return ((left_height > right_height) ? (left_height + 1) : (right_height + 1));
+	return ((left_height > right_height) ?
+			(left_height + 1) : (right_height + 1));
 }
 /**
  * binary_tree_balance - determines if binary tree is balanced
